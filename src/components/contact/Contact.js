511a1dacdfdd597fbba7e110930c032edf7347a4
unknown <marcin.czaja2@gmail.com>
2020-09-17 19:41:42 +0200

    upgraded form css

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -5,9 +5,12 @@ export default function Contact() {
     <div>
       <div id="contact">
         <div className="contact_sidebar">
-            <p className="sidebar_description">
-                Zapraszam do kontaktu. Zobaczymy czy będę w stanie Państwu pomóc.
-            </p>
+          <h3 className="sidebar_headline">Zapraszam do kontaktu.</h3>
+          <p className="sidebar_description">
+            Niezależnie czy mają Państwo pomysł na stronę lub szukają gotowego
+            projektu, dla każdego postaram się coś znaleźć. Zapraszam do
+            kontaktu.
+          </p>
         </div>
         <div className="contact_main">
           <form action="" className="contact_form">
@@ -18,13 +21,18 @@ export default function Contact() {
                 placeholder="This is an early development version..."
                 name="message"
                 id="message"
+                rows="15"
               ></textarea>
             </div>
             <div className="form_formUser">
-              <label htmlFor="name">Twoje imię</label>
-              <input type="text" name="name" id="name" />
-              <label htmlFor="mail">Twoj email</label>
-              <input type="email" name="mail" id="mail" />
+              <div className="formUser_name">
+                <label htmlFor="name">Twoje imię</label>
+                <input type="text" name="name" id="name" />
+              </div>
+              <div className="formUser_mail">
+                <label htmlFor="mail">Twoj email</label>
+                <input type="email" name="mail" id="mail" />
+              </div>
             </div>
             <button>Wyślij</button>
           </form>
